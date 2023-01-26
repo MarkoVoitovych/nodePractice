a6c5064b9f509109adccf9a5511e41802689e436
MarkoVoitovych <searchangeos@gmail.com>
2023-01-26 16:46:44 +0200

    practice end

--- index.js
+++ index.js
@@ -5,14 +5,15 @@
 // process.nextTick(() => {
 //   console.log('Next tick callback');
 // });
-
+const fs = require('fs').promises;
+console.log(fs);
 (async () => {
-  const fs = require('fs').promises;
   const thisDir = await fs.readdir(__dirname);
   console.log(thisDir);
   const statistics = await Promise.all(
     thisDir.map(async fileName => {
       const stats = await fs.stat(fileName);
+      //   console.dir(stats);
       return {
         Name: fileName,
         Size: stats.size,
