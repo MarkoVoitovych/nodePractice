d7d828009636a3e94ea93a2341780297eb749052
MarkoVoitovych <searchangeos@gmail.com>
2023-01-29 13:07:18 +0200

    CLI application finished

--- index.js
+++ index.js
@@ -1,25 +1,67 @@
-// const { fn } = require('./develop');
-// fn();
-// const fn = () => console.log(process.cwd());
-
-// process.nextTick(() => {
-//   console.log('Next tick callback');
-// });
+const readline = require('readline');
 const fs = require('fs').promises;
-console.log(fs);
-(async () => {
-  const thisDir = await fs.readdir(__dirname);
-  console.log(thisDir);
-  const statistics = await Promise.all(
-    thisDir.map(async fileName => {
-      const stats = await fs.stat(fileName);
-      //   console.dir(stats);
-      return {
-        Name: fileName,
-        Size: stats.size,
-        Date: stats.mtime,
-      };
-    }),
+const { program } = require('commander');
+require('colors');
+
+program.option(
+  '-f, --file <type>',
+  'file for saving game results',
+  'results.txt',
+);
+program.parse(process.argv);
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+let count = 0;
+const logFile = program.opts().file;
+const mind = Math.floor(Math.random() * 10) + 1;
+
+const isValid = value => {
+  if (isNaN(value)) {
+    console.log('Введіть число!'.red);
+    return false;
+  }
+  if (value < 1 || value > 10) {
+    console.log('Число має бути в діапазоні від 1 до 10'.red);
+    return false;
+  }
+  return true;
+};
+
+const log = async data => {
+  try {
+    await fs.appendFile(logFile, `${data}\n`);
+    console.log(`Вдалося зберегти результат у файл ${logFile}`.green);
+  } catch (err) {
+    console.log(`Не вдалося зберегти файл ${logFile}`.red);
+  }
+};
+
+const game = () => {
+  rl.question(
+    'Введіть число від 1 до 10, щоб вгадати задумане: '.yellow,
+    value => {
+      let a = +value;
+      if (!isValid(a)) {
+        game();
+        return;
+      }
+      count += 1;
+      if (a === mind) {
+        console.log('Вітаю Ви вгадали число за %d крок(ів)'.green, count);
+        log(
+          `${new Date().toLocaleDateString()}: Вітаю Ви вгадали число за ${count} крок(ів)`,
+        ).finally(() => rl.close());
+        return;
+      }
+      console.log('Ви не вгадали ще спроба'.red);
+
+      game();
+    },
   );
-  console.table(statistics);
-})();
+};
+
+game();
